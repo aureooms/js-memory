1c246a2611c90ee033df5af641397c1f2f6a40d8
Ooms Aurélien <aurelien.ooms@gmail.com>
2014-11-29 14:35:15 +0100

    test and fix pool class

--- js/src/calloc.js
+++ js/src/calloc.js
@@ -1,13 +1,13 @@
 
 
 /**
- * Method that creates an allocator from a constructor.
+ * Method that creates an allocator from an array constructor.
  */
 
-var __calloc__ = function(Constructor) {
-	return function(n) {
-		return new Constructor(n);
-	};
-};
+var __calloc__ = function ( ArrayConstructor ) {
+	return function ( n ) {
+		return new ArrayConstructor( n ) ;
+	} ;
+} ;
 
-exports.__calloc__ = __calloc__;
+exports.__calloc__ = __calloc__ ;
